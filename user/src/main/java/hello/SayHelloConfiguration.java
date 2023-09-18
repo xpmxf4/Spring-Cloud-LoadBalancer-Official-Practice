915f77d7cc409b0272bf73c9afa9f67b4076eaab
dev-chpark <xpmxf5@gmail.com>
2023-09-18 22:21:20 +0900

    에제 done

---
+++ user/src/main/java/hello/SayHelloConfiguration.java
@@ -0,0 +1,41 @@
+package hello;
+
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SayHelloConfiguration {
+
+    @Bean
+    @Primary
+    ServiceInstanceListSupplier serviceInstanceListSupplier() {
+        return new DemoServiceInstanceSuppler("say-hello");
+    }
+}
+
+class DemoServiceInstanceSuppler implements ServiceInstanceListSupplier {
+    private final String serviceId;
+
+    public DemoServiceInstanceSuppler(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    @Override
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    @Override
+    public Flux<List<ServiceInstance>> get() {
+        return Flux.just(Arrays
+                .asList(new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 8090, false),
+                        new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 9092, false),
+                        new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 9999, false)));
+    }
+}
---
+++ user/src/main/java/hello/UserApplication.java
@@ -0,0 +1,43 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class UserApplication {
+
+	private final WebClient.Builder loadBalancedWebClientBuilder;
+	private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	public UserApplication(WebClient.Builder webClientBuilder, ReactorLoadBalancerExchangeFilterFunction lbFunction) {
+		this.loadBalancedWebClientBuilder = webClientBuilder;
+		this.lbFunction = lbFunction;
+	}
+
+	@RequestMapping("/hi")
+	public Mono<String> hi(@RequestParam(value = "name", defaultValue = "Mary") String name) {
+		return loadBalancedWebClientBuilder.build().get().uri("http://say-hello/greeting")
+				.retrieve().bodyToMono(String.class)
+				.map(greeting -> String.format("%s %s", greeting, name));
+	}
+
+	@RequestMapping("/hello")
+	public Mono<String> hello(@RequestParam(value = "name", defaultValue = "John") String name) {
+		return WebClient.builder()
+				.filter(lbFunction)
+				.build().get().uri("http://say-hello/greeting")
+				.retrieve().bodyToMono(String.class)
+				.map(greeting -> String.format("%s %s!", greeting, name));
+	}
+}
